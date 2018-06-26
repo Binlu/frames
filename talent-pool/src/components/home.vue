d10e87e5360b110526ffdf22dc617d07427377a8
Binlu <826202368@qq.com>
2018-06-26 11:39:40 +0800

    人才库提交

---
+++ talent-pool/src/components/home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    	<h1>极地-人才库</h1>
+
+    	<p>{{msg}}</p>
+
+
+    	<router-link to='/list'>立即进入》</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        data () {
+            return{msg: '欢迎来的极地人才库'}
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ talent-pool/src/components/list.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    	<h1>极地-人才库</h1>
+
+    	<ul>
+            <li>111</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        data () {
+            return{msg: '欢迎来的极地人才库'}
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
