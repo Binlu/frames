d252bf1245c31e7df1e49c381581bb1aeace5a64
Binlu <826202368@qq.com>
2018-06-27 16:41:49 +0800

    新增编辑页面提交

--- talent-pool/src/pages/add.vue
+++ talent-pool/src/pages/add.vue
@@ -3,17 +3,20 @@
         <h3>基本信息</h3>
         <div class="row">
             <div class="col-md-3">
-                <img class="head-pic" :src="item.pic" alt="">
+                <button id="upload-file"><img class="head-pic" :src="item.pic" alt=""></button>
             </div>
             <div class="row col-md-9">
-                <div class="col-md-4">
-                    姓名：***
+                <div class="col-md-6">
+                    <label>姓名：</label>
+                    <input  v-model="item.name" placeholder="请输入姓名">
+                </div>
+                <div class="col-md-6">
+                    <label>学历：</label>
+                    <input v-model="item.xueli" placeholder="请输入学历">
                 </div>
                 <div class="col-md-4">
-                    学历：本科
-                </div>
-                <div class="col-md-4">
-                    专业：经济管理
+                    <label>专业：</label>
+                    <input v-model="item.zhuanye" placeholder="请输入专业">
                 </div>
                 <div class="col-md-4">
                     毕业院校：上海交大
@@ -64,13 +67,21 @@
             </div>
 
         </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <button class="btn" @click=subFunc(this)>提交</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import {mapState, mapGetters, mapActions} from 'vuex'
+    import 'layui-src/dist/layui.all'
+    import 'layui-src/dist/css/layui.css'
     export default {
-        name: 'detail',
+        name: 'add',
         data () {
             return{
                 info_data:{}
@@ -80,22 +91,44 @@
             ...mapState(['listMsg']),
             ...mapGetters(['allLists']),
             item(){
+
                 const obj = this.allLists.find( item => item.id == this.$route.params.id ) || {};
                 return obj;
             }
         },
         methods: {
             ...mapActions(['getLists']),
-        },
-        created() {
-            console.log(11111)
-            if(!this.allLists.length){
-                this.getLists();  //获取列表数据
+            subFunc:function(){
+                console.log(this.item)
             }
         },
+        created() {
+            if(this.$route.query.id!=undefined){
+                if(!this.allLists.length){
+                    this.getLists();  //获取列表数据
+                }
+            }
+        },
+        updated(){
+        },
         mounted() {
-            console.log(this.$route.params.id)
-            console.log(this.item)
+
+            // 绑定事件
+            layui.use('upload', function(){
+                var upload = layui.upload;
+               
+                //执行实例
+                var uploadInst = upload.render({
+                    elem: '#upload-file' //绑定元素
+                    ,url: '/upload/' //上传接口
+                    ,done: function(res){
+                        //上传完毕回调
+                    }
+                    ,error: function(){
+                        //请求异常回调
+                    }
+                });
+            });
         },
         
     }
